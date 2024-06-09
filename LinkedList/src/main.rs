bc2465a24e41ed63bf810c510e69c98cabeb0cc5
Sourav Pundir <singhsourav3@hotmail.com>
2024-06-09 17:36:36 +0530

    Refined Linked list data struct
    1. More meaningful version of linkedlist
    2. Added pop() method to take out last element

--- LinkedList/src/main.rs
+++ LinkedList/src/main.rs
@@ -1,31 +1,72 @@
 #[derive(Debug)]
-pub struct ListNode {
-    pub val: i32,
-    pub next: Option<Box<ListNode>>,
+pub struct LinkedList {
+    head: Option<Box<Node>>,
+}
+#[derive(Debug)]
+struct Node {
+    val: i32,
+    next: Option<Box<Node>>,
 }
 
-impl ListNode {
+impl Node {
     #[inline]
     fn new(val: i32) -> Self {
-        ListNode { next: None, val }
+        Node { next: None, val }
+    }
+}
+
+impl LinkedList {
+    fn new() -> Self {
+        LinkedList { head: None }
+    }
+    fn add_node(&mut self, value: i32) {
+        match self.head.as_mut() {
+            None => {
+                self.head = Some(Box::new(Node::new(value)));
+            }
+            Some(first_node) => {
+                let mut temp = first_node;
+                while let Some(ref mut x) = temp.next {
+                    temp = x;
+                }
+                temp.next = Some(Box::new(Node::new(value)));
+            }
+        }
     }
 
-    fn add_node(&mut self , value: i32) {
-
-        let mut temp = self;
-
-        while let Some(ref mut x) = temp.next {
-          temp = x;
+    fn pop(&mut self) -> Option<i32> {
+        if self.head.as_ref().is_none() {
+            return None;
         }
-        temp.next = Some(Box::new(ListNode::new(value))); 
+
+        if self.head.as_ref().unwrap().next.is_some() {
+            let mut temp = self.head.as_mut();
+            while temp
+                .as_mut()
+                .unwrap()
+                .next
+                .as_ref()
+                .map_or(false, |node| node.next.is_some())
+            {
+                temp = temp.unwrap().next.as_mut();
+            }
+
+            let ret_val = Some(temp.as_ref().unwrap().next.as_ref().unwrap().val);
+            temp.as_mut().unwrap().next = None;
+            ret_val
+        } else {
+            let ret_val = Some(self.head.as_ref().unwrap().val);
+            self.head = None;
+            ret_val
+        }
     }
 }
 // taken from https://users.rust-lang.org/t/a-problem-about-pointer/32319
-pub fn delete_duplicates(head: Option<ListNode>) -> Option<ListNode> {
+pub fn delete_duplicates(head: Option<Node>) -> Option<Node> {
     match head {
         Some(mut node) => {
             let mut value = node.val;
-            let mut ptr =  &mut node;
+            let mut ptr = &mut node;
 
             /* while let Some(ref mut n) = ptr.next {
                 if value == n.val {
@@ -39,34 +80,46 @@ pub fn delete_duplicates(head: Option<ListNode>) -> Option<ListNode> {
                 }
             } */
             while ptr.next.is_some() {
-                if ptr.next.as_ref().map_or(false, |n| n.val == value){
+                if ptr.next.as_ref().map_or(false, |n| n.val == value) {
                     ptr.next = ptr.next.as_mut().and_then(|n| n.next.take());
                 } else {
                     value = ptr.next.as_ref().unwrap().val;
                     ptr = ptr.next.as_mut().unwrap();
                 }
-            } 
+            }
             Some(node)
         }
-        None => { 
-            println!("None reached"); 
+        None => {
+            println!("None reached");
             None
         }
     }
 }
 fn main() {
-    let mut head = ListNode::new(5);
-    head.add_node(5);
-    head.add_node(6);
-    head.add_node(6);
-    head.add_node(6);
-    head.add_node(7);
-    head.add_node(7);
-    println!("List is {:?}", head);
+    let mut list = LinkedList::new();
+    list.add_node(5);
+    list.add_node(6);
+    list.add_node(6);
+    list.add_node(6);
+    list.add_node(7);
+    list.add_node(7);
+    println!("List is {:#?}", list);
     //let mut head = Some(Box::new(ListNode::new(4)));
-    let mut head = Some(head);
-    head = delete_duplicates(head);
+    //let mut head = Some(head);
+    //head = delete_duplicates(head);
     //let mut head = Some(Box::new(head));
     //delete_duplicates(&mut head);
-    println!("List is {:#?}", head.unwrap());
+    println!("======================================");
+    println!("deleted node:{}", list.pop().unwrap());
+    println!("deleted node:{}", list.pop().unwrap());
+    println!("deleted node:{}", list.pop().unwrap());
+    println!("deleted node:{}", list.pop().unwrap());
+    println!("deleted node:{}", list.pop().unwrap());
+    println!("deleted node:{}", list.pop().unwrap());
+    //println!("deleted node:{}", list.pop().unwrap());
+
+    println!("Remaining List is:");
+    println!("{:#?}", list);
+    //println!("deleted node:{}", list.pop().unwrap());
+    println!("======================================");
 }
